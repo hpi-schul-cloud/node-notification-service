73dbb62d91e5d531aeb58814536a068cb401c269
Tobias Wollowski <tobias.wollowski@student.hpi.de>
2016-11-30 14:33:44 +0100

    added constants for notification states and device types

--- src/services/constants.js
+++ src/services/constants.js
@@ -9,6 +9,22 @@ class Constants {
       CLICKED : 'clicked'
     }
 
+    this.NOTIFICATION_STATES = {
+      CREATED: 'created',
+      ESCALATING: 'escalating',
+      ESCAlATED: 'escalated',
+      SEEN: 'seen',
+      CLICKED: 'clicked',
+      REMOVED: 'removed'
+    }
+
+    this.DEVICE_TYPES = {
+      //ACTIVE: 'active',
+      DESKTOP: 'desktop',
+      MOBILE: 'mobile',
+      EMAIL: 'email'
+    }
+
   }
 
 
