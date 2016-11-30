5667c366d194e5f5f14e519e90d75040409c945b
Jan Kohstall <jan.kohstall@gmail.com>
2016-11-30 15:31:44 +0100

    Registration for devices.

--- src/services/device/index.js
+++ src/services/device/index.js
@@ -2,64 +2,60 @@
 
 const hooks = require('./hooks');
 const device = require('../user/index');
+const resolve = require('../resolve/index');
 
 class Service {
+
   constructor(options) {
     this.options = options || {};
   }
 
-  find(params) {
-    return Promise.resolve([]);
-  }
-
-  get(id, params) {
-    return Promise.resolve({
-      id, text: `A new message with ID: ${id}!`
-    });
-  }
-
-
   // Adds a device for a user to the database
-  create(user, token, id, service, type, name, OS) {
+  create(data, params) {
 
     // Create device object
     const newDevice = new device({
-      token: token,
-      type: type,
-      service: service,
-      name: name,
-      OS: OS,
+      token: data.token,
+      type: data.type,
+      service: data.service,
+      name: data.name,
+      OS: data.OS,
       state: 'registered'
     });
 
+    // Resolve user only existing users can register in the Schul-Cloud app
+    var userId = resolve.Service.verifyUser(data.user);
+
     // Create User
     const newUser = new user({
-      name: user,
+      name: userId,
       devices: newDevice
-
     });
 
     // Insert data into DB
-    new Promise((resolve, reject) => {
-      newUser.save()
+    return new Promise((resolve, reject) => {
+
+      user.findOne({_id:userId})
+        .then( user => {
+          user.devices.push(newDevice);
+          return user.save();
+        })
+        .then ( userWithNewDevice => {
+          resolve(userWithNewDevice);
+        })
+        .catch( err => {
+          console.log(err);
+          return newUser.save();
+        })
+        .then( userNew => {
+          resolve(userNew);
+        })
         .catch( err => {
           reject(new errors.GeneralError('Internal Error'));
         })
+
     });
   }
-
-/*
-  update(id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  patch(id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  remove(id, params) {
-    return Promise.resolve({ id });
-  }*/
 }
 
 module.exports = function(){
