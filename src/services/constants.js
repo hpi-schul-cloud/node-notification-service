7a5694b840feecebf0a6068a4681400339551220
Jan Silbersiepe <jan.silbersiepe@motionintelligence.net>
2016-11-30 13:30:19 +0100

    added constants class

---
+++ src/services/constants.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class Constants {
+
+  constructor(options) {
+
+    this.CALLBACK_TYPES = {
+      RECEIVED : 'received',
+      CLICKED : 'clicked'
+    }
+
+  }
+
+
+}
+
+module.exports = new Constants();
