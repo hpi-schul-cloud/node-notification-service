2d1f46ea26dc1eb9d663f4ae6ff914ad4ab12531
Felix Musmann <fmusmann@gmail.com>
2016-11-30 15:38:47 +0100

    return aggregated promise for all services

--- src/services/sendInterface/index.js
+++ src/services/sendInterface/index.js
@@ -1,5 +1,4 @@
 'use strict';
-const _ = require('lodash');
 const notificationServices = {
   firebase: require('./adapters/firebase')
 };
@@ -9,25 +8,71 @@ class SendInterface {
   /**
    * This method sends the given notifications to all devices via the appropriate service.
    * If multiple notifications are given, only the message of the first is respected.
-   * The returned promise resolves, if at least one notification was sent.
+   * The returned promise resolves, if at least one notification was sent successfully.
    * The returned promise rejects, if all notifications fail.
    */
   send(notifications, devices) {
+    return new Promise((resolve, reject) => {
+      let results = {};
 
-    let results = {};
+      // group devices and notifications by service
+      let groupsByService = this._groupByService(notifications, devices);
+      let allPromises = [];
 
-    // group devices by service
-    let devicesByService = _.groupBy(devices, device => device.service);
+      for (let service in groupsByService) {
+        let serviceNotifications = groupsByService[service].notifications;
+        let serviceDevices = groupsByService[service].devices;
 
-    _.forEach(devicesByService, (service) => {
-      if (!_.has(notificationServices, service)) {
-        // there is no interface for this service
-        // TODO: handle error
-        continue;
+        if (notificationServices.hasOwnProperty(service)) {
+          let promise = notificationServices[service].send(serviceNotifications, serviceDevices);
+          allPromises.push(promise);
+        } else {
+          // there is no interface for this service
+          // TODO: handle error
+        }
       }
 
-      notificationServices[service].send(notifications, devices);
-    })
+      Promise.all(allPromises).then((responses) => {
+        let accumulatedResponses = {
+          success: 0,
+          failure: 0,
+          results: []
+        };
+
+        responses.forEach((response) => {
+          this._addResponse(accumulatedResponses, response);
+        });
+
+        if (accumulatedResponses.failure == notifications.length) {
+          reject(accumulatedResponses);
+        } else {
+          resolve(accumulatedResponses);
+        }
+      });
+    });
+  }
+
+  _addResponse(accumulatedResponses, response) {
+    accumulatedResponses.success += response.success;
+    accumulatedResponses.failure += response.failure;
+    accumulatedResponses.results = accumulatedResponses.results.concat(response.results);
+    return accumulatedResponses;
+  }
+
+  _groupByService(notifications, devices) {
+    return devices.reduce((groupsByService, device, index) => {
+      if (!groupsByService.hasOwnProperty(device.service)) {
+        groupsByService[device.service] = {
+          notifications: [],
+          devices: []
+        };
+      }
+
+      groupsByService[device.service].notifications.push(notifications[index]);
+      groupsByService[device.service].devices.push(device);
+
+      return groupsByService;
+    }, {});
   }
 
 }
