ad052c6d359a18ace92f9c3dfd7771be36b7796d
Lars Lange <langleu90@gmail.com>
2016-12-05 10:46:23 +0100

    Update firebase.js

--- src/services/sendInterface/adapters/firebase.js
+++ src/services/sendInterface/adapters/firebase.js
@@ -1,5 +1,5 @@
 'use strict';
-const config = require('./config.json').firebase;
+const config = require('./config.sample.json').firebase;
 const firebase = require('node-gcm');
 const errors = require('feathers-errors');
 
