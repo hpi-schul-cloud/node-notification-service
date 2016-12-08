2e752b37bef93b92507abc2f6178626e726cea5c
Jan Silbersiepe <jan.silbersiepe@motionintelligence.net>
2016-12-08 11:01:40 +0100

    changed message model (authToken to initiatorId)

--- src/services/message/message-model.js
+++ src/services/message/message-model.js
@@ -11,14 +11,14 @@ const Constants = require('../constants');
 const Util = require('../util');
 
 const messageSchema = new Schema({
+
   title: { type: String, required: true },
   body: { type: String, required: true },
   action: { type: String, required: false },
   priority: { type: String, default: Constants.MESSAGE_PRIORITIES.MEDIUM, enum: Util.getEnumValues(Constants.MESSAGE_PRIORITIES) },
   timeToLive: { type: Date, required: false },
 
-  authToken: { type: String, required: true },
-  userId: { type: String, required: true },
+  initiatorId: { type: String, required: true }, // ID (nicht token!!!) von dem service oder dem user der die message geschickt hat
   scopeIds: { type: [String], required: true },
 
   userIds: { type: [String], required: false },
