346c347c683c76555acbd78a4562a91f56a9920e
Tobias Wollowski <tobias@jbbr.net>
2017-01-04 14:02:38 +0100

    [WIP] send notifications based on escalation model

--- src/services/orchestration/index.js
+++ src/services/orchestration/index.js
@@ -2,43 +2,90 @@
 
 const sendInterface = require('../sendInterface');
 const User = require('../user/user-model');
+const Notification = require('../notification/notification-model');
+const Escalation = require('./escalation-model');
+
+const Constants = require('../constants');
+const Util = require('../util');
 
 class Orchestration {
 
-  escalate(notification) {
-    // TODO insert escalation magic
+  escalate(escalation) {
+    debugger;
+    return User
+      .findOne({
+        schulcloudId: escalation.notification.user
+      })
+      .then(user => {
+        //if (!user) reject("user not found");
+        let news = [];
+        let devices = user.devices.filter(function (device) {
+          return device.type === escalation.nextEscalationType;
+        });
+        if (devices && devices.length) {
+          console.log(devices.length, "devices found...");
+          for (var i = 0; i < devices; i++) {
+            news.push(escalation.notification);
+          }
+          sendInterface.send(news, devices)
+            .then(res => {
+              console.log('[INFO] notification sent');
+              return this.updateEscalation(escalation);
+            })
+            .catch(err => {
+              console.log('[ERROR] send error');
+            })
+        }else{
+          console.log('no devices found...');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  updateEscalation(escalation) {
+    switch (escalation.nextEscalationType) {
+      case Constants.DEVICE_TYPES.DESKTOP:
+        escalation.nextEscalationType = Constants.DEVICE_TYPES.MOBILE;
+        break;
+      case Constants.DEVICE_TYPES.MOBILE:
+        escalation.nextEscalationType = Constants.DEVICE_TYPES.EMAIL;
+        break;
+      case Constants.DEVICE_TYPES.EMAIL:
+      default:
+        escalation.nextEscalationType = "NONE";
+    }
+    if (escalation.nextEscalationType === "NONE") {
+      return escalation.delete();
+    } else {
+      escalation.nextEscalationDue = Date.now;
+      return escalation.save();
+    }
   }
 
   orchestrate(notifications) {
 
     console.log('[INFO] orchestrating notification');
-
     notifications.forEach((notification) => {
-      notification.changeState('orchestrated');
+      notification.changeState(Constants.NOTIFICATION_STATES.ESCALATING);
+      notification.save();
     });
 
-    return new Promise((resolve, reject) => {
-      return User
-        .findOne({
-          schulcloudId: notifications[0].user
-        })
-        .then( user => {
-          // send to all of users devices
-          let news = [];
-          for(var i = 0; i < user.devices.length; i++) {
-            news.push(notifications[0]);
-          }
-          sendInterface.send(news, user.devices)
-            .then( res => {
-              console.log('[INFO] notification sent');
-              resolve(res);
-            })
-            .catch( err => {
-              console.log('[ERROR] send error');
-              reject();
-            })
-          });
+    let notification = notifications[0];
+
+    //return new Promise((resolve, reject) => {
+
+    let escalation = Escalation({
+      notification: notification,
+      priority: notification.priority,
+      nextEscalationType: Constants.DEVICE_TYPES.DESKTOP,
+      nextEscalationDue: Date.now
     });
+    // escalate
+    return this.escalate(escalation);
+
+    //});
 
   }
 
