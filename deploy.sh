0dd631c8a6b7f2362b66a23106a127fe13225f6d
Jan Silbersiepe <jan.silbersiepe@motionintelligence.net>
2017-01-04 17:43:14 +0100

    improved slack notification

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ git pull
 npm install
 forever restart src/index.js
 githash=$(git log --pretty=format:'%h' -n 1)
-curl -X POST -d "{'attachments':[{'fallback':'New deploy live', 'pretext':'', 'color':'#009245', 'fields':[{'title':'New Deploy', 'value':'Commit #$githash is now live.', 'short':false } ] } ] } }" https://hooks.slack.com/services/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
+name=$(git --no-pager show -s --format='%an' $githash)
+curl -X POST -d "{'attachments':[{'fallback':'New deploy live', 'pretext':'', 'color':'#009245', 'fields':[{'title':'New Deploy', 'value':'Commit <https://github.com/schulcloud/node-notification-service/commit/$githash|$githash> by $name is now live.', 'short':false } ] } ] } }" https://hooks.slack.com/services/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
 
