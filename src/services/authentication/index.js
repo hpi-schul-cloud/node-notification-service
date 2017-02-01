115c9b231ef87f30cd7dcacbcb74514b0d93ada9
Jan Silbersiepe <jan.silbersiepe@motionintelligence.net>
2017-02-01 17:05:23 +0100

    unix stringify...

--- src/services/authentication/index.js
+++ src/services/authentication/index.js
@@ -10,7 +10,7 @@ class Authentication {
 
     return function(hook) {
 
-      console.log('[AUTH] ' + JSON.stringify(hook));
+      // console.log('[AUTH] ' + JSON.stringify(hook));
 
       let token;
 
