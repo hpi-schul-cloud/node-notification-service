c83150a2b758ed050f539c922f664b40fb99cdd2
hefowe <hendrik.folkerts@student.hpi.uni-potsdam.de>
2018-06-14 13:51:44 +0200

    Add pushRouter to App

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mailRouter from './routes/mail';
+import pushRouter from './routes/push';
 
 const app: express.Application = express();
 const port: string = process.env.PORT || '3000';
@@ -9,6 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/mails', mailRouter);
+app.use('/push', pushRouter);
 
 app.get('/', function (req, res) {
   res.send('hello world!');
---
+++ src/routes/push.ts
@@ -0,0 +1,29 @@
+import admin from 'firebase-admin';
+import express from 'express';
+import PushService from '../services/PushService';
+
+const router: express.Router = express.Router();
+const pushService: PushService = new PushService();
+
+router.post('/', (req, res) => {
+  const push: admin.messaging.Message = {
+    token: req.body.token,
+    data: req.body.data,
+    notification: req.body.notification,
+    android: req.body.android,
+    webpush: req.body.webpush,
+    apns: req.body.apns
+  };
+
+  pushService.send(req.body.platformId, push)
+    .then((response: any) => {
+      console.log(response);
+    })
+    .catch((e: Error) => {
+      console.log('Error: ' + e);
+    });
+
+    res.send('Push queued.');
+});
+
+export default router;
