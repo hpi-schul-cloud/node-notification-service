dc6c5c181663115df9f91a3a36adaf6e9fa301e7
hefowe <hendrik.folkerts@student.hpi.uni-potsdam.de>
2018-06-22 09:57:44 +0200

    Add basic escalation logic

--- src/services/EscalationLogic.ts
+++ src/services/EscalationLogic.ts
@@ -1,3 +1,8 @@
+import TemplatingService from "@/services/TemplatingService";
+import MailService from "@/services/MailService";
+import PushService from "@/services/PushService";
+import Message from "@/interfaces/Message";
+
 export default class EscalationLogic {
   // region public static methods
   // endregion
@@ -9,12 +14,50 @@ export default class EscalationLogic {
   // endregion
 
   // region private members
+
+  private mailService: MailService;
+  private pushService: PushService;
+
   // endregion
 
   // region constructor
+
+  public constructor() {
+    this.mailService = new MailService();
+    this.pushService = new PushService();
+  }
+
   // endregion
 
   // region public methods
+
+  public escalate(messageId: number) {
+    // const MessageModel = mongoose.model('Message', messageSchema);
+    // MessageModel.findById(messageId, (err, message) => {
+      // message ...
+    // });
+
+    const message: Message = { platform: '', template: '', content: {}, payload: [], receivers: [] };
+    
+    // TODO: Move this check to somewehere else
+    if (typeof message.receivers === 'string') {
+      message.receivers = [];
+    }
+
+    // Construct Templating Service
+    const templatingService: TemplatingService = new TemplatingService(message.platform, message.template, message.payload, message.content);
+
+    // Construct message for every single User
+    for (const receiver of message.receivers) {
+      const pushMessage = templatingService.createPushMessage(receiver);
+      this.pushService.send(message.platform, pushMessage);
+
+      // TODO: Send Mail Message after Timeout depending on notification settings
+      const mailMessage = templatingService.createMailMessage(receiver);
+      this.mailService.send(message.platform, mailMessage);
+    }
+  }
+
   // endregion
 
   // region private methods
