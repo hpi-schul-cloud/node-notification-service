d9d3661ff654e97a3f4052963797d4ed2a412eae
hefowe <hendrik.folkerts@student.hpi.uni-potsdam.de>
2018-06-22 09:38:14 +0200

    Add helper function getTemplate to Utils

--- src/utils.ts
+++ src/utils.ts
@@ -3,4 +3,9 @@ export default class Utils {
         const config = require(`../platforms/${platformId}/config.json`);
         return config;
     }
+
+    public static getTemplate(platformId: string, templateId: string, type: string): any {
+        const config = require(`../platforms/${platformId}/templates/${templateId}/${type}.json`);
+        return config;
+    }
 }
