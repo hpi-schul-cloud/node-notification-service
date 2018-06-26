02b3832ef864acfd4465d5004ce6a1cb5a38f887
Christian Friedow <christian@friedow.com>
2018-06-26 11:45:05 +0200

    Added user pagination test api endpoint

--- src/app.ts
+++ src/app.ts
@@ -9,18 +9,65 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
 
-const app: express.Application = express();
-const port: string = process.env.PORT || '3000';
+  const app: express.Application = express();
+  const port: string = process.env.PORT || '3000';
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
 
-app.use('/mails', mailRouter);
-app.use('/push', pushRouter);
-app.use('/messages', messageRouter);
+  app.use('/mails', mailRouter);
+  app.use('/push', pushRouter);
+  app.use('/messages', messageRouter);
 
-app.get('/', function (req, res) {
+  app.get('/', function (req, res) {
     res.send('hello world!');
+  });
+
+  // Test Endpoint for user pagination
+  app.get('/users', function (req, res) {
+    const users = [
+      {
+        name: "Bob",
+        mail: "bob@bob.bob",
+        language: "en",
+        payload: {
+          course: "Bobs Course",
+          week: 6
+        }
+      },
+      {
+        name: "Alice",
+        mail: "alice@alice.alice",
+        language: "de",
+        payload: {
+          course: "Alices Course",
+          week: 4
+        }
+      }
+    ];
+
+    if (!req.query.page) {
+      res.json({
+        data: users,
+      });
+      return;
+    }
+
+    if (req.query.page >= users.length) {
+      res.json({
+        data: [],
+      });
+      return;
+    }
+
+    const links = {
+      next: `http://localhost:3000/users?page=${ parseInt(req.query.page) + 1 }`,
+    };
+
+    res.json({
+      data: [users[req.query.page]],
+      links: req.query.page + 1 >= users.length ? {} : links,
+    });
   });
 
   app.listen(port, () => {
