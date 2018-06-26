1ba45da6241bc431d62ca2f300232cfa72c4b7a1
Christian Friedow <christian@friedow.com>
2018-06-26 11:31:54 +0200

    Added database connection to server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
+import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mailRouter from './routes/mail';
 import pushRouter from './routes/push';
 import messageRouter from './routes/message';
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
 
 const app: express.Application = express();
 const port: string = process.env.PORT || '3000';
@@ -15,9 +20,13 @@ app.use('/push', pushRouter);
 app.use('/messages', messageRouter);
 
 app.get('/', function (req, res) {
-  res.send('hello world!');
+    res.send('hello world!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/`);
+  });
+
 });
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/`);
-});
+mongoose.connect('mongodb://localhost/notification-service');
