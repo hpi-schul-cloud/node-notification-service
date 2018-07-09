e0053025ffb863de011a8909f5854292b5959e8e
Christian Friedow <christian@friedow.com>
2018-07-09 11:20:41 +0200

    Added yaml loader

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,12 @@
+import yaml from 'js-yaml';
+import fs from 'fs';
+
 export default class Utils {
     public static getPlatformConfig(platformId: string): any {
         return require(`../platforms/${platformId}/config.json`);
     }
 
     public static getTemplate(platformId: string, templateId: string, type: string): any {
-        return require(`../platforms/${platformId}/templates/${templateId}/${type}.json`);
+        return yaml.safeLoad(fs.readFileSync(`../platforms/${platformId}/templates/${templateId}/${type}.mustache`, 'utf8'));
     }
 }
