5804e9e7733892847ab46e4831f55369eda35a91
Christian Friedow <christian@friedow.com>
2018-07-09 19:04:38 +0200

    loaded mustache templates

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,17 @@
 import yaml from 'js-yaml';
 import fs from 'fs';
+import path from 'path';
+import Template from '@/interfaces/Template';
 
 export default class Utils {
-    public static getPlatformConfig(platformId: string): any {
-        return require(`../platforms/${platformId}/config.json`);
-    }
+  public static getPlatformConfig(platformId: string): any {
+    return require(`../platforms/${platformId}/config.json`);
+  }
 
-    public static getTemplate(platformId: string, templateId: string, type: string): any {
-        return yaml.safeLoad(fs.readFileSync(`../platforms/${platformId}/templates/${templateId}/${type}.mustache`, 'utf8'));
-    }
+  public static loadTemplate(platformId: string, templateId: string, type: string): Template {
+    const templatePath = path.join(__dirname, '..', 'platforms', platformId, 'templates', templateId, `${type}.mustache`);
+    const template = yaml.safeLoad(fs.readFileSync(templatePath, 'utf8'));
+    template.type = type;
+    return template;
+  }
 }
