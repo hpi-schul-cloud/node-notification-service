a07b2affc237e698b137a5e82b79114a2dfe0d17
Hendrik Folkerts <hendrik.folkerts@student.hpi.uni-potsdam.de>
2018-07-30 17:17:06 +0200

    Add device test data

---
+++ test/data/device.ts
@@ -0,0 +1,11 @@
+import Device from '@/interfaces/Device';
+
+const device: Device = {
+  platform: 'testplatform',
+  mail: 'receiver@test.test',
+  tokens: [
+    'nyht4ca81bGam26a',
+  ],
+};
+
+export default device;
