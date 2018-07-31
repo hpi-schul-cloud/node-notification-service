b6edc886debb3b2635b3a67c500db5ea81b473ae
Hendrik Folkerts <hendrik.folkerts@student.hpi.uni-potsdam.de>
2018-07-31 12:16:30 +0200

    Add TestUtils class

---
+++ test/test-utils.ts
@@ -0,0 +1,7 @@
+export default class TestUtils {
+    public static timeout(delay: number) {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(), delay);
+        });
+    }
+}
