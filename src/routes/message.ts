0549c5522adb9d20cb042db7190d2964e0450987
Tobias Wollowski <tobias.wollowski@student.hpi.de>
2019-01-23 14:43:43 +0100

    fix missing property

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -28,6 +28,7 @@ router.post('/', (req, res) => {
     payload: req.body.payload,
     languagePayloads: req.body.languagePayloads,
     receivers: req.body.receivers,
+    seen: [],
   };
 
   if (req.body.sender) {
