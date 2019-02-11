a09d9c200bfcf2533c5e5ba8a2004c2112a7e8f0
Tobias Wollowski <tobias.wollowski@student.hpi.de>
2019-02-11 08:50:15 +0100

    export app

--- src/app.ts
+++ src/app.ts
@@ -7,84 +7,88 @@ import pushRouter from '@/routes/push';
 import messageRouter from '@/routes/message';
 import deviceRouter from '@/routes/device';
 
-function startApiServer() {
-  const app: express.Application = express();
-  const port: string = process.env.NOTIFICATION_PORT || '3000';
+const app: express.Application = express();
 
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+// function startApiServer(app: express.Application) {
 
-  app.use('/mails', mailRouter);
-  app.use('/push', pushRouter);
-  app.use('/messages', messageRouter);
-  app.use('/devices', deviceRouter);
+const port: string = process.env.NOTIFICATION_PORT || '3000';
 
-  app.get('/', (req, res) => {
-    res.send('hello world!');
-  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../swagger.json')));
+app.use('/mails', mailRouter);
+app.use('/push', pushRouter);
+app.use('/messages', messageRouter);
+app.use('/devices', deviceRouter);
 
-  // Test Endpoint for user pagination
-  app.get('/users', (req, res) => {
-    const users = [
-      {
-        name: 'Bob',
-        mail: 'bob@bob.bob',
-        payload: {
-          course: 'Bobs Course',
-          week: 6,
-        },
-        language: 'en',
-        preferences: {
-          push: true,
-          mail: true,
-        },
+app.get('/', (req, res) => {
+  res.send('hello world!');
+});
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../swagger.json')));
+
+// Test Endpoint for user pagination
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      name: 'Bob',
+      mail: 'bob@bob.bob',
+      payload: {
+        course: 'Bobs Course',
+        week: 6,
       },
-      {
-        name: 'Alice',
-        mail: 'alice@alice.alice',
-        payload: {
-          course: 'Alices Course',
-          week: 4,
-        },
-        language: 'de',
-        preferences: {
-          push: true,
-          mail: true,
-        },
+      language: 'en',
+      preferences: {
+        push: true,
+        mail: true,
       },
-    ];
+    },
+    {
+      name: 'Alice',
+      mail: 'alice@alice.alice',
+      payload: {
+        course: 'Alices Course',
+        week: 4,
+      },
+      language: 'de',
+      preferences: {
+        push: true,
+        mail: true,
+      },
+    },
+  ];
 
-    if (!req.query.page) {
-      res.json({
-        data: users,
-      });
-      return;
-    }
-
-    if (req.query.page >= users.length) {
-      res.json({
-        data: [],
-      });
-      return;
-    }
-
-    const links = {
-      next: `http://localhost:3000/users?page=${parseInt(req.query.page, 10) + 1}`,
-    };
-
+  if (!req.query.page) {
     res.json({
-      data: [users[req.query.page]],
-      links: req.query.page + 1 >= users.length ? {} : links,
+      data: users,
     });
-  });
+    return;
+  }
 
-  app.listen(port);
-}
+  if (req.query.page >= users.length) {
+    res.json({
+      data: [],
+    });
+    return;
+  }
+
+  const links = {
+    next: `http://localhost:3000/users?page=${parseInt(req.query.page, 10) + 1}`,
+  };
+
+  res.json({
+    data: [users[req.query.page]],
+    links: req.query.page + 1 >= users.length ? {} : links,
+  });
+});
+
+app.listen(port);
+// }
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', startApiServer);
+// db.once('open', () => { startApiServer(app) });
 
 mongoose.connect(`mongodb://${process.env.MONGO_HOST || 'localhost'}/notification-service`);
+
+export default app;
