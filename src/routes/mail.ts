a4adec1a6851da58804913e1859bc2d08f00a4f7
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2020-01-07 11:53:07 +0100

    rough sketch of how it can work

--- src/routes/mail.ts
+++ src/routes/mail.ts
@@ -2,18 +2,38 @@ import express from 'express';
 import MailService from '@/services/MailService';
 import Mail from '@/interfaces/Mail';
 import logger from '@/helper/logger';
+import TemplatingService from '@/services/TemplatingService';
+import UserResource from '@/interfaces/UserResource';
+import Utils from '@/utils';
 
 const router = express.Router();
 const mailService = new MailService();
 
 
-router.post('/', (req, res) => {
-	const mail: Mail = {
-		to: req.body.to,
-		subject: req.body.subject,
-		text: req.body.text,
-		html: req.body.html,
+router.post('/', async (req, res) => {
+	let templatingService: TemplatingService;
+	const messageId = Utils.guid();
+
+	let receiver: UserResource = {
+		mail: req.body.to,
+		language: 'de',
+		payload: {},
+		name: '',
+		userId: '',
+		preferences: {}
 	};
+
+	templatingService = await TemplatingService.create(req.body.platform, req.body.template,
+		req.body.payload, req.body.languagePayloads, messageId, receiver.language);
+
+	const mail = await templatingService.createMailMessage(receiver);
+
+	// const mail: Mail = {
+	// 	to: req.body.to,
+	// 	subject: req.body.subject,
+	// 	text: req.body.text,
+	// 	html: req.body.html,
+	// };
 
 	if (req.body.from) {
 		mail.from = req.body.from;
