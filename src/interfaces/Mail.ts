a314b3d6d26082aa5d825660b996a76461e2efdd
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-02-11 12:19:58 +0100

    add replyTo? to mail interface

--- src/interfaces/Mail.ts
+++ src/interfaces/Mail.ts
@@ -1,8 +1,9 @@
 export default interface Mail {
 	from?: string;
+	replyTo?: string;
 	to: string;
 	subject: string;
 	text: string;
 	html: string;
-	attachments?: Array<{content: Buffer|string, filename: string}>;
+	attachments?: Array<{ content: Buffer | string, filename: string }>;
 }
--- test/data/mail.ts
+++ test/data/mail.ts
@@ -3,6 +3,7 @@ import Mail from '@/interfaces/Mail';
 const mail: Mail = {
 	from: 'sender@test.test',
 	to: 'receiver@test.test',
+	replyTo: 'reply@domain.tld',
 	subject: 'Test Subject',
 	text: 'Test Plaintext',
 	html: '<html>Test HTML</html>',
