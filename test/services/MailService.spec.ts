fc07e144b51715d0b9b4f26066928ed2ea5d0f13
Tobias W <tofixx@users.noreply.github.com>
2020-02-18 16:50:36 +0100

    Feature/use mailcatcher for tests (#137)
    * Update nodejs.yml
    
    * Update nodejs.yml
    
    * Update nodejs.yml
    
    * Update nodejs.yml
    
    * added mongodb
    
    * use mailcatcher by default for tests, ethereal email is currently not working
    
    * add nvmrc file
    
    * remove duplication
    
    * add mailcatcher
    
    * update travis config
    
    * nvm is already used
    
    * exit tests

--- test/services/MailService.spec.ts
+++ test/services/MailService.spec.ts
@@ -4,6 +4,34 @@ import nodeMailer from 'nodemailer';
 import MailService from '@/services/MailService';
 import mail from '@test/data/mail';
 import message from '@test/data/message';
+import logger from '@/helper/logger';
+import { doesIntersect } from 'tslint';
+
+const EMAIL_SERVICE: string = process.env.MAIL_SERVICE || 'mailcatcher';
+
+const SMTP_MAILCATCHER: any = {
+	smtp: {
+		host: process.env.MAILCATCHER_HOST || 'localhost',
+		port: 1025,
+		secure: false
+	},
+	user: '',
+	pass: '',
+};
+
+const getTestEmailAccount = async ({ EMAIL_SERVICE }: any) => {
+	let account: any = null;
+	if (EMAIL_SERVICE === 'ethereal') {
+		account = await nodeMailer.createTestAccount();
+		logger.info('Use Ethereal Email: https://ethereal.email/',
+			{ smpt: account.smtp, user: account.user, pass: account.pass }
+		);
+	} else {
+		account = SMTP_MAILCATCHER;
+		logger.info('using mailcatcheer for receiving emails in tests')
+	}
+	return account;
+}
 
 describe('MailService.send', () => {
 
@@ -13,16 +41,16 @@ describe('MailService.send', () => {
 
 	before('should send an mail.', async () => {
 		// Create ethereal mail account
-		const account: any = await nodeMailer.createTestAccount();
-		// console.log({ smpt: account.smtp, user: account.user, pass: account.pass });
+		const account: any = await getTestEmailAccount({ EMAIL_SERVICE });
 		const { host, port, secure } = account.smtp;
+		const { user, pass } = account;
 		const transporter = nodeMailer.createTransport({
 			host,
 			port,
 			secure,
 			auth: {
-				user: account.user,
-				pass: account.pass
+				user,
+				pass
 			}
 		});
 
