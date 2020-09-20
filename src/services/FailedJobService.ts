10ab91a4c8e1117da8759418b57f9246c90f8f31
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-09-20 11:02:01 +0200

    Add endpoints for shd and fix nodemon

--- src/services/FailedJobService.ts
+++
@@ -1,8 +0,0 @@
-import FailedJobModel from '@/models/failedJobs';
-import logger from '@/helper/logger';
-
-export default class FailedJobService {
-	public getJobByReceiver(receiver: string) {
-		return FailedJobModel.find({ receiver }).lean().exec();
-	}
-}
